cdb242738d62b91e813da8592cdef7429140e4c4
Tony Galmiche <tony.galmiche@infosaone.com>
2019-03-10 11:50:59 +0100

    Mise en place signature mail

--- is_utilisateur.py
+++ is_utilisateur.py
@@ -10,8 +10,9 @@ class is_site(models.Model):
     _description = "Site"
     _order='name'
 
-    name             = fields.Char('Site', required=True)
-    code             = fields.Char('Code', required=True)
+    name             = fields.Char(u'Site', required=True)
+    code             = fields.Char(u'Code', required=True)
+    signature_mail   = fields.Html(u'Modèle signature mail',sanitize=False)
     dest_mozilla_ids = fields.Many2many('is.utilisateur', 'is_site_utilisateur_rel', 'ris_site_id','utilisateur_id', string="Destinataires des anomalies des sauvegardes de Mozilla")
 
 
@@ -46,11 +47,87 @@ class is_utilisateur(models.Model):
     autre           = fields.Char('Autre')
     commentaire     = fields.Text('Commentaire')
     action_ids      = fields.One2many('is.action', 'utilisateur_id', u'Actions', readonly=True)
+    signature_mail  = fields.Html(u'Signature mail',sanitize=False)
     active          = fields.Boolean('Actif', default=True)
 
 
     @api.multi
     def generer_signature_mail(self):
         for obj in self:
-            print(obj)
+            html=obj.site_id.signature_mail
+            fonction  = obj.fonction  or ''
+            telephone = obj.telephone or ''
+            portable  = obj.portable  or ''
+            fax       = obj.fax       or ''
+            autre     = obj.autre     or ''
+            if telephone != '':
+                telephone = u'Tél : '+telephone
+            if portable != '':
+                portable = u'Mobile : '+portable
+            if fax != '':
+                fax = u'Fax : '+fax
+            html = html.replace('[name]'     , (obj.name or ''))
+            html = html.replace('[fonction]' , fonction)
+            html = html.replace('[telephone]', telephone)
+            html = html.replace('[portable]' , portable)
+            html = html.replace('[fax]'      , fax)
+            html = html.replace('[autre]'    , autre)
+            if html:
+                obj.signature_mail = html
+
+
+    @api.multi
+    def envoyer_signature_mail(self):
+        for obj in self:
+            name = 'signature-mail.html'
+            path = '/tmp/' + name
+            f = open(path,'wb')
+            f.write(obj.signature_mail.encode('utf-8'))
+            f.close()
+            datas = open(path,'rb').read().encode('base64')
+
+            # ** Recherche si une pièce jointe est déja associèe ***************
+            attachment_obj = self.env['ir.attachment']
+            model=self._name
+            #name='commandes.pdf'
+            attachments = attachment_obj.search([('res_model','=',model),('res_id','=',obj.id),('name','=',name)])
+            # ******************************************************************
+
+            # ** Creation ou modification de la pièce jointe *******************
+            vals = {
+                'name':        name,
+                'datas_fname': name,
+                'type':        'binary',
+                'res_model':   model,
+                'res_id':      obj.id,
+                'datas':       datas,
+            }
+            if attachments:
+                for attachment in attachments:
+                    attachment.write(vals)
+            else:
+                attachment = attachment_obj.create(vals)
+            subject=u'['+obj.name+u'] Nouvelle signature de mail'
+            email_to=obj.mail
+            user  = self.env['res.users'].browse(self._uid)
+            email_from = user.email
+            nom   = user.name
+            body_html=u"""
+                <p>Bonjour,</p>
+                <p>Veuillez trouver ci-joint une nouvelle signature pour votre mail.</p>
+                <p>Merci de mettre en place celle-ci dans Thunderbird.</p>
+                <p>"""+nom+u"""</p>
+            """
+            vals={
+                'email_from'    : email_from, 
+                'email_to'      : email_to, 
+                'email_cc'      : email_from,
+                'subject'       : subject,
+                'body_html'     : body_html,
+                'attachment_ids': [(6, 0, [attachment.id])] 
+            }
+            email=self.env['mail.mail'].create(vals)
+            if email:
+                self.env['mail.mail'].send(email)
+
 
