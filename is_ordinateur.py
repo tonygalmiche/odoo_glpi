bd1654fd53ea84d49a27ee6bd34cdaa6cd2e71ad
Tony Galmiche <tony.galmiche@infosaone.com>
2022-04-12 09:01:17 +0200

    modifs

--- is_ordinateur.py
+++ is_ordinateur.py
@@ -99,7 +99,7 @@ class is_ordinateur(models.Model):
         ct=0
         for obj in self:
             ct=ct+1
-            _logger.info(str(ct)+'/'+str(nb)+u' : Mise à jour GLPI '+str(obj.name))
+            _logger.info(str(ct)+u'/'+str(nb)+u' : Mise à jour GLPI '+str(obj.name))
 #            SQL="""
 #                SELECT 
 #                    c.name,
